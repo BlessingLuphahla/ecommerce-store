48829cedcc94623cf809a6ee0ecaf72107bb321a
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2024-08-27 14:09:44 +0200

    files update

---
+++ frontend/src/Components/assets/data/all_products.js
@@ -0,0 +1,191 @@
+import p1_img from "../images/product (1).jpg";
+import p10_img from "../images/product (10).jpg";
+import p11_img from "../images/product (11).jpg";
+import p12_img from "../images/product (12).jpg";
+import p13_img from "../images/product (13).jpg";
+import p14_img from "../images/product (14).jpg";
+import p15_img from "../images/product (15).jpg";
+import p16_img from "../images/product (16).jpg";
+import p17_img from "../images/product (17).jpg";
+import p18_img from "../images/product (18).jpg";
+import p19_img from "../images/product (19).jpg";
+import p2_img from "../images/product (2).jpg";
+import p20_img from "../images/product (20).jpg";
+import p21_img from "../images/product (21).jpg";
+import p3_img from "../images/product (3).jpg";
+import p4_img from "../images/product (4).jpg";
+import p5_img from "../images/product (5).jpg";
+import p6_img from "../images/product (6).jpg";
+import p7_img from "../images/product (7).jpg";
+import p8_img from "../images/product (8).jpg";
+import p9_img from "../images/product (9).jpg";
+
+let data_product = [
+  {
+    id: 1,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p1_img,
+    new_price: 64,
+    old_price: 21,
+  },
+
+  {
+    id: 10,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p10_img,
+    new_price: 56,
+    old_price: 32,
+  },
+
+  {
+    id: 11,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p11_img,
+    new_price: 63,
+    old_price: 17,
+  },
+
+  {
+    id: 12,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p12_img,
+    new_price: 32,
+    old_price: 29,
+  },
+
+  {
+    id: 13,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p13_img,
+    new_price: 27,
+    old_price: 48,
+  },
+
+  {
+    id: 14,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p14_img,
+    new_price: 21,
+    old_price: 47,
+  },
+
+  {
+    id: 15,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p15_img,
+    new_price: 27,
+    old_price: 12,
+  },
+
+  {
+    id: 16,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p16_img,
+    new_price: 37,
+    old_price: 46,
+  },
+
+  {
+    id: 17,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p17_img,
+    new_price: 60,
+    old_price: 44,
+  },
+
+  {
+    id: 18,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p18_img,
+    new_price: 20,
+    old_price: 45,
+  },
+
+  {
+    id: 19,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p19_img,
+    new_price: 20,
+    old_price: 31,
+  },
+
+  {
+    id: 2,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p2_img,
+    new_price: 46,
+    old_price: 40,
+  },
+
+  {
+    id: 20,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p20_img,
+    new_price: 57,
+    old_price: 37,
+  },
+
+  {
+    id: 21,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p21_img,
+    new_price: 50,
+    old_price: 47,
+  },
+
+  {
+    id: 3,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p3_img,
+    new_price: 21,
+    old_price: 25,
+  },
+
+  {
+    id: 4,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p4_img,
+    new_price: 59,
+    old_price: 12,
+  },
+
+  {
+    id: 5,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p5_img,
+    new_price: 50,
+    old_price: 30,
+  },
+
+  {
+    id: 6,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p6_img,
+    new_price: 67,
+    old_price: 29,
+  },
+
+  {
+    id: 7,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p7_img,
+    new_price: 65,
+    old_price: 48,
+  },
+
+  {
+    id: 8,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p8_img,
+    new_price: 37,
+    old_price: 10,
+  },
+
+  {
+    id: 9,
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
+    image: p9_img,
+    new_price: 49,
+    old_price: 15,
+  },
+];
--- frontend/src/Components/assets/data/data.js
+++ frontend/src/Components/assets/data/data.js
@@ -1,33 +1,33 @@
-import p1_img from "../images/product (1).jpg";
-import p2_img from "../images/product (2).jpg";
-import p3_img from "../images/product (3).jpg";
-import p4_img from "../images/product (4).jpg";
+import p1_img from "../images/product (8).jpg";
+import p2_img from "../images/product (14).jpg";
+import p3_img from "../images/product (15).jpg";
+import p4_img from "../images/product (19).jpg";
 
 let data_product = [
   {
     id: 1,
-    name: "I am a random ass name ....this is not creative at all",
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
     image: p1_img,
     new_price: 50.0,
     old_price: 80.5,
   },
   {
     id: 2,
-    name: "I am a random ass name ....this is not creative at all",
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
     image: p2_img,
     new_price: 150.0,
     old_price: 80.5,
   },
   {
     id: 3,
-    name: "I am a random ass name ....this is not creative at all",
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
     image: p3_img,
     new_price: 50.0,
     old_price: 180.5,
   },
   {
     id: 4,
-    name: "I am a random ass name ....this is not creative at all",
+    name: "Random ass name.. this is not creative at all.I mean i aint got time to write things one by one",
     image: p4_img,
     new_price: 60.0,
     old_price: 80.5,
